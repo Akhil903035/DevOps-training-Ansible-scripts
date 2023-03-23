33f6c1c31c1b1dc7ed679d41fdfe45eec0145dbc
Akhil kumar Sathuluri <akhilkumarsathuluri@Akhils-Air-3.lan>
2023-03-23 12:03:12 -0500

    Homework

---
+++ Powershell/forcondition.ps1
@@ -0,0 +1,3 @@
+for($a = 10; $a -lt 15;$a++)
+{ $a = $a*2
+write-host $a}
---
+++ Powershell/ifcondition.ps1
@@ -0,0 +1,4 @@
+$value1=10
+$value2=20
+if($value1 -eq $value2) {write-host $value1 "is equal to" $value2}
+else{write-host $value1" is not equal to" $value2}
